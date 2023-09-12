beef8561f48926c491ce5d8bd525887a6230d19d
Mohamad Dbouk <114245392+mhdbouk@users.noreply.github.com>
2023-09-12 15:15:25 +0300

    Add support for local multiplayer and refactor + cool style

--- Components/GameBoardComponent.razor.cs
+++ Components/GameBoardComponent.razor.cs
@@ -1,236 +1,68 @@
 using Microsoft.AspNetCore.Components;
-using System.Security.Cryptography;
 using TicTacToe.Constants;
+using TicTacToe.Core;
 
 namespace TicTacToe.Components;
 
 public partial class GameBoardComponent : ComponentBase
 {
-    [Inject]
-    public NavigationManager NavigationManager { get; set; } = default!;
     [Parameter]
     public GameType GameType { get; set; }
 
-    private List<string> _events = new List<string>() { $"Started @ {DateTime.Now}"};
-    private string?[,] _board = new string[3, 3];
-    private List<(int Row, int Col)> _winningMoves = new List<(int Row, int Col)>();
-    private string? _winner = null;
-    private bool _gameOver = false;
+    [Inject]
+    public NavigationManager NavigationManager { get; set; } = default!;
+    
+    private TicTacToeBoard _board = new TicTacToeBoard();
+    private Player _currentTurn = Player.Undefined;
 
+    protected override void OnInitialized()
+    {
+        _currentTurn = Player.X;
+    }
+    
     private void GoBack()
     {
         NavigationManager.NavigateTo("/");
     }
-    
-    private string IsWinnerCell(int row, int col)
-    {
-        if (!_gameOver)
-        {
-            return string.Empty;
-        }
 
-        if (CheckWinner(row, col))
-        {
-            return "winning-cell";
-        }
-
-        return string.Empty;
-    }
-    
     private void BoxClicked(int row, int col)
     {
-        if (_board[row, col]?.Length > 0)
+        if (_board.IsGameOver() || _board.GetCellPlayer(row, col) is Player.O or Player.X)
         {
-            _events.Add($"Box already clicked at {row},{col} with Value {_board[row, col]}");
             return;
         }
 
-        if (_gameOver)
+        _board.SetCell(row, col, _currentTurn);
+        if (_board.IsGameOver())
         {
             return;
         }
-        
-        _board[row, col] = "X";
-        _events.Add($"Box clicked at {row},{col}");
-        if (IsGameOver())
-        {
-            
-            return;
-        }
-        
+
         NextTurn();
     }
-
-    private bool IsGameOver()
-    {
-        var winningPlayer = GetWinningPlayer();
-
-        if (!string.IsNullOrEmpty(winningPlayer))
-        {
-            _winner = $"{winningPlayer} Wins!";
-            _events.Add(_winner);
-            _gameOver = true;
-            return true;
-        }
-        if (CheckTie())
-        {
-            _winner = "Tie!";
-            _events.Add(_winner);
-            _gameOver = true;
-            return true;
-        }
-
-        return false;
-    }
-    
-    private string? GetWinningPlayer()
-    {
-        if (CheckWinner("X"))
-        {
-            return "X";
-        }
-
-        if (CheckWinner("O"))
-        {
-            return "O";
-        }
-
-        return null;
-    }
-    
     private void NextTurn()
     {
-        if (IsGameOver())
+        switch (GameType)
         {
-            return;
+            case GameType.AI:
+                _currentTurn = _currentTurn == Player.X ? Player.O : Player.X;
+                var (row, col) = _board.GetAiNextMove(_currentTurn);
+                _board.SetCell(row, col, _currentTurn);
+                _currentTurn = _currentTurn == Player.X ? Player.O : Player.X;
+                break;
+            case GameType.TwoPlayer:
+                _currentTurn = _currentTurn == Player.X ? Player.O : Player.X;
+                break;
         }
-
-        var (row, col) = GetWinningMove("O");
-        if ((row, col) == (null, null))
-        {
-            (row, col) = GetWinningMove("X");
-            if ((row, col) == (null, null))
-            {
-                (row, col) = RandomTurn();
-            }
-        }
-        
-        _events.Add($"AI placed O at {row},{col}");
-        _board[row!.Value, col!.Value] = "O";
-
-        IsGameOver();
     }
-    
-    private (int? row, int? col) GetWinningMove(string player)
+
+    private string IsWinnerCell(int row, int col)
     {
-        for (var row = 0; row < 3; row++)
-        {
-            for (var col = 0; col < 3; col++)
-            {
-                if (_board[row, col] == null && IsWinningMove(player, row, col))
-                {
-                    return (row, col);
-                }
-            }
-        }
-
-        return (null, null);
+        return _board.IsGameOver() && _board.IsWinnerCell(row, col) ? "winning-cell" : "";
     }
-
-    private bool IsWinningMove(string player, int row, int col)
+    private void RestartGame()
     {
-        _board[row, col] = player;
-        var isWinningMove = CheckWinner(player);
-        _board[row, col] = null; // Reset the move regardless of win or not
-        return isWinningMove;
+        _board.Clear();
+        _currentTurn = Player.X;
     }
-
-    private (int row, int col) RandomTurn()
-    {
-        var row = RandomNumberGenerator.GetInt32(0, 3);
-        var col = RandomNumberGenerator.GetInt32(0, 3);
-        return _board[row, col] == null ? (row, col) : RandomTurn();
-    }
-
-    private bool CheckTie()
-    {
-        for (var row = 0; row < 3; row++)
-        {
-            for (var col = 0; col < 3; col++)
-            {
-                if (_board[row, col] == null)
-                {
-                    return false;
-                }
-            }
-        }
-        _winningMoves.Clear();
-        return true;
-    }
-
-    private bool CheckWinner(int row, int col)
-        => _winningMoves.Contains((row, col));
-
-    private bool CheckWinner(string player)
-    {
-        return CheckRows(player) || CheckColumns(player) || CheckDiagonals(player);
-    }
-    
-    private bool CheckRows(string player)
-    {
-        for (var row = 0; row < 3; row++)
-        {
-            if (_board[row, 0] == player && _board[row, 1] == player && _board[row, 2] == player)
-            {
-                _winningMoves.Clear();
-                _winningMoves.Add((row, 0));
-                _winningMoves.Add((row, 1));
-                _winningMoves.Add((row, 2));
-                return true;
-            }
-        }
-        return false;
-    }
-    
-    private bool CheckColumns(string player)
-    {
-        for (var col = 0; col < 3; col++)
-        {
-            if (_board[0, col] == player && _board[1, col] == player && _board[2, col] == player)
-            {
-                _winningMoves.Clear();
-                _winningMoves.Add((0, col));
-                _winningMoves.Add((1, col));
-                _winningMoves.Add((2, col));
-
-                return true;
-            }
-        }
-        return false;
-    }
-    
-    private bool CheckDiagonals(string player)
-    {
-
-        if (_board[0, 0] == player && _board[1, 1] == player && _board[2, 2] == player)
-        {
-            _winningMoves.Clear();
-            _winningMoves.Add((0, 0));
-            _winningMoves.Add((1, 1));
-            _winningMoves.Add((2, 2));
-            return true;
-        }
-        if (_board[0, 2] == player && _board[1, 1] == player && _board[2, 0] == player)
-        {
-            _winningMoves.Clear();
-            _winningMoves.Add((0, 2));
-            _winningMoves.Add((1, 1));
-            _winningMoves.Add((2, 0));
-            return true;
-        }
-
-        return false;
-
-    }
-
 }
---
+++ Core/TicTacToeBoard.cs
@@ -0,0 +1,199 @@
+using System.Security.Cryptography;
+using TicTacToe.Constants;
+
+namespace TicTacToe.Core;
+
+public class TicTacToeBoard
+{
+    private readonly Cell[,] _cells;
+    private List<(int Row, int Col)> _winningMoves = default!;
+    
+    public TicTacToeBoard()
+    {
+        _cells = new Cell[3, 3];
+        InitializeBoard();
+    }
+    public string? Winner { get; private set; } = null;
+
+    private void InitializeBoard()
+    {
+        Winner = null;
+        _winningMoves = new List<(int Row, int Col)>();
+        for (int row = 0; row < 3; row++)
+        {
+            for (int col = 0; col < 3; col++)
+            {
+                _cells[row, col] = new Cell();
+            }
+        }
+    }
+
+    public Player? GetCellPlayer(int row, int col)
+    {
+        var player = _cells[row, col].Player;
+        return player is Player.Undefined ? null : player;
+    }
+
+    public bool IsWinnerCell(int row, int col)
+        => _winningMoves.Contains((row, col));
+
+    public bool IsGameOver()
+    {
+        var winningPlayer = GetWinningPlayer();
+
+        if (winningPlayer is not null)
+        {
+            Winner = winningPlayer.ToString();
+            return true;
+        }
+        if (CheckTie())
+        {
+            Winner = "Tie";
+            return true;
+        }
+
+        return false;
+    }
+    
+    private Player? GetWinningPlayer()
+    {
+        if (CheckWinner(Player.X))
+        {
+            return Player.X;
+        }
+
+        if (CheckWinner(Player.O))
+        {
+            return Player.O;
+        }
+
+        return null;
+    }
+    public void SetCell(int row, int col, Player player)
+        => _cells[row, col].Player = player;
+    
+    private bool CheckTie()
+    {
+        for (var row = 0; row < 3; row++)
+        {
+            for (var col = 0; col < 3; col++)
+            {
+                if (_cells[row, col].Player == Player.Undefined)
+                {
+                    return false;
+                }
+            }
+        }
+        _winningMoves.Clear();
+        return true;
+    }
+    
+    public (int row, int col) GetAiNextMove(Player player)
+    {
+        var (row, col) = GetWinningMove(player);
+        if ((row, col) != (null, null)) return (row!.Value, col!.Value);
+
+        (row, col) = GetWinningMove(player == Player.O ? Player.X : Player.O);
+        if ((row, col) == (null, null))
+        {
+            (row, col) = RandomTurn();
+        }
+
+        return (row!.Value, col!.Value);
+    }
+    private (int row, int col) RandomTurn()
+    {
+        var row = RandomNumberGenerator.GetInt32(0, 3);
+        var col = RandomNumberGenerator.GetInt32(0, 3);
+        return _cells[row, col].Player == Player.Undefined ? (row, col) : RandomTurn();
+    }
+
+    private (int? row, int? col) GetWinningMove(Player player)
+    {
+        for (var row = 0; row < 3; row++)
+        {
+            for (var col = 0; col < 3; col++)
+            {
+                if (_cells[row, col].Player == Player.Undefined && IsWinningMove(player, row, col))
+                {
+                    return (row, col);
+                }
+            }
+        }
+
+        return (null, null);
+    }
+    private bool IsWinningMove(Player player, int row, int col)
+    {
+        _cells[row, col].Player = player;
+        var isWinningMove = CheckWinner(player);
+        _cells[row, col].Player = Player.Undefined; // Reset the move regardless of win or not
+        return isWinningMove;
+    }
+
+    private bool CheckWinner(Player player)
+    {
+        return CheckRows(player) || CheckColumns(player) || CheckDiagonals(player);
+    }
+    
+    private bool CheckRows(Player player)
+    {
+        for (var row = 0; row < 3; row++)
+        {
+            if (_cells[row, 0].Player == player && _cells[row, 1].Player == player && _cells[row, 2].Player == player)
+            {
+                _winningMoves.Clear();
+                _winningMoves.Add((row, 0));
+                _winningMoves.Add((row, 1));
+                _winningMoves.Add((row, 2));
+                return true;
+            }
+        }
+        return false;
+    }
+    
+    private bool CheckColumns(Player player)
+    {
+        for (var col = 0; col < 3; col++)
+        {
+            if (_cells[0, col].Player == player && _cells[1, col].Player == player && _cells[2, col].Player == player)
+            {
+                _winningMoves.Clear();
+                _winningMoves.Add((0, col));
+                _winningMoves.Add((1, col));
+                _winningMoves.Add((2, col));
+
+                return true;
+            }
+        }
+        return false;
+    }
+    
+    private bool CheckDiagonals(Player player)
+    {
+
+        if (_cells[0, 0].Player == player && _cells[1, 1].Player == player && _cells[2, 2].Player == player)
+        {
+            _winningMoves.Clear();
+            _winningMoves.Add((0, 0));
+            _winningMoves.Add((1, 1));
+            _winningMoves.Add((2, 2));
+            return true;
+        }
+        if (_cells[0, 2].Player == player && _cells[1, 1].Player == player && _cells[2, 0].Player == player)
+        {
+            _winningMoves.Clear();
+            _winningMoves.Add((0, 2));
+            _winningMoves.Add((1, 1));
+            _winningMoves.Add((2, 0));
+            return true;
+        }
+
+        return false;
+
+    }
+    public void Clear()
+    {
+        InitializeBoard();
+    }
+}
